0369378477850ade3a7ee03925e166fea3e64d74
mrota89 <marcello.rota.1989@gmail.com>
2021-02-10 17:46:15 +0100

    milestone 2 completata

--- js/script.js
+++ js/script.js
@@ -5,27 +5,57 @@ new Vue({
     query:'',
     resultFor: '',
     listaFilm: [],
+    listaSerie: [],
     maxVote: 5
   },
 
   methods: {
-    cercaFilm: function() {
+    searchAll: function() {
+      this.searchMovie();
+      this.searchSeries();
+    },
+
+    searchMovie: function() {
       const self = this;
       axios
-      .get(`https://api.themoviedb.org/3/search/movie?api_key=87afaf40f86102cb8e49027ba59c133d&query=${self.query}`)
+      .get(('https://api.themoviedb.org/3/search/movie'), {
+        params: {
+          api_key: '87afaf40f86102cb8e49027ba59c133d',
+          query: self.query,
+          language: 'it-IT'
+        }
+      })
       .then(function(xhr) {
         let dataObject = xhr.data;
         self.listaFilm = dataObject.results;
-        self.voteFive();
-        self.resultFor = `risultati per: ${self.query}`
-        self.query = "";
-      })
+        self.voteFive(self.listaFilm);
+      });
     },
-    voteFive: function() {
+
+    searchSeries: function() {
+      const self = this;
+      axios
+      .get(('https://api.themoviedb.org/3/search/tv'), {
+        params: {
+          api_key: '87afaf40f86102cb8e49027ba59c133d',
+          query: self.query,
+          language: 'it-IT'
+        }
+      })
+      .then(function(xhr) {
+        let dataObject = xhr.data;
+        self.listaSerie = dataObject.results;
+        self.voteFive(self.listaSerie);
+        self.resultFor = `risultati per: ${self.query}`;
+        self.query = "";
+      });
+    },
+
+    voteFive: function(lista) {
       let round;
       let whiteStar;
       let number_ws;
-      this.listaFilm.forEach((element) => {
+      lista.forEach((element) => {
         const {vote_average} = element
         round = Math.round(element.vote_average / 2);
         element.vote_average = round;
@@ -33,8 +63,9 @@ new Vue({
         element.number_ws = whiteStars
       });
     },
-    flagLang: function(index) {
-      const flagImage = this.listaFilm[index].original_language;
+
+    flagLang: function(index, lista) {
+      const flagImage = lista[index].original_language;
       const imageString = `flag-svg/${flagImage}.svg`;
       return imageString;
     }
